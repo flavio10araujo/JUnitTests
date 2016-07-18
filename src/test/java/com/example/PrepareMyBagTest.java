fcb270599b18e4e70f9445e8c0379834d7117e37
flavio10araujo <flavio10araujo@yahoo.com.br>
2016-07-18 19:20:37 -0300

    Test

---
+++ src/test/java/com/example/PrepareMyBagTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrepareMyBagTest {
+
+	FirstDayAtSchool school = new FirstDayAtSchool();
+	String[] bag = { "Books", "Notebooks", "Pens" };
+	
+	@Test
+	public void testPrepareMyBag() {
+		System.out.println("Inside testPrepareMyBag()");
+		assertArrayEquals(bag, school.prepareMyBag());
+	}
+}
